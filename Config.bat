7f198169819b40d587500824d29f53a00f9902c3
Nekonekoo <patrickfetscher60@gmail.com>
2020-05-26 21:21:27 +0200

    .

--- Config.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title TERA Proxy Config
-cd /d "%~dp0"
-
-bin\node.exe --use-strict bin\configurator
--- mods/bugfix/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs'),
-	path = require('path')
-
-const fixesDir = path.join(__dirname, 'fixes'),
-	fixes = fs.readdirSync(fixesDir).map(name => require(path.join(fixesDir, name)))
-
-module.exports = function Bugfix(mod) {
-	for(let SubMod of fixes) new SubMod(mod)
-}
